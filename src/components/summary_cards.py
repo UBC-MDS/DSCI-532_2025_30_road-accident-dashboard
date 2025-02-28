979e12b404d766c717c7ecbccdd6291d823499e7
Franklin Aryee <faryee@student.ubc.ca>
2025-02-28 01:44:29 -0800

    style: reformat file for PEP8 compliance and improved readability

--- src/components/summary_cards.py
+++ src/components/summary_cards.py
@@ -1,43 +1,22 @@
 import dash_bootstrap_components as dbc
 
-color='white'
+color = "white"
 
-style = {
-    'height': 220
-}
+style = {"height": 220}
 
-card_total_acc = dbc.Card(
-    color=color,
-    style=style,
-    id="card-total-acc"
-)
-card_total_fatal = dbc.Card(
-    color=color,
-    style=style,
-    id="card-total-fatal"
-)
-card_avg_resp = dbc.Card(
-    color=color,
-    style=style,
-    id="card-avg-resp"
-)
-card_total_eco_loss = dbc.Card(
-    
-    color=color,
-    style=style,
-    id="card-total-eco-loss"
-)
+card_total_acc = dbc.Card(color=color, style=style, id="card-total-acc")
+card_total_fatal = dbc.Card(color=color, style=style, id="card-total-fatal")
+card_avg_resp = dbc.Card(color=color, style=style, id="card-avg-resp")
+card_total_eco_loss = dbc.Card(color=color, style=style, id="card-total-eco-loss")
 
-card_leading_cause = dbc.Card(
-    color=color,
-    style=style,
-    id="card-leading-cause"
-)
+card_leading_cause = dbc.Card(color=color, style=style, id="card-leading-cause")
 
-summary_cards = dbc.Row([
-    dbc.Col(card_total_acc),
-    dbc.Col(card_total_fatal),
-    dbc.Col(card_avg_resp),
-    dbc.Col(card_total_eco_loss),
-    dbc.Col(card_leading_cause)
-])
+summary_cards = dbc.Row(
+    [
+        dbc.Col(card_total_acc),
+        dbc.Col(card_total_fatal),
+        dbc.Col(card_avg_resp),
+        dbc.Col(card_total_eco_loss),
+        dbc.Col(card_leading_cause),
+    ]
+)
